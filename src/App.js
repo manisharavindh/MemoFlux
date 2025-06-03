3823eb60aeadbc30a314ca00733816bd09063d36
Manish Aravindh <manisharavindh2007@gmail.com>
2025-06-03 15:54:27 +0530

    v0.1.3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [isDark, setIsDark] = useState(true);
+  const [isDark, setIsDark] = useState(() => {
+    const savedTheme = localStorage.getItem('isDark');
+    return savedTheme !== null ? JSON.parse(savedTheme) : true;
+  });
 
   const toggleTheme = () => {
     setIsDark(!isDark);
   };
+
+  useEffect(() => {
+    localStorage.setItem('isDark', JSON.stringify(isDark));
+  }, [isDark]);
 
   return (
     <div className={`App ${isDark ? 'dark' : 'light'}`}>
@@ -17,7 +24,12 @@ function App() {
           className={`toggle-button ${isDark ? 'dark' : 'light'}`}
         >
         </button>
-        <h1>Initializing MemoFlux</h1>
+        <div className="nav">
+          <div className='title'>[<span className='logo'>memoflux</span>]{/*  <span className='links'>[Links]</span>*/}[<span className='todo'>todo</span>]</div>
+        </div>
+        <div className="content">
+          <p>Links will be implemented here</p>
+        </div>
       </div>
     </div>
   );
