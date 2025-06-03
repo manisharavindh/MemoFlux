fe1c4190e13c97b3b25624e6eb77a73541625e06
Manish Aravindh <manisharavindh2007@gmail.com>
2025-06-03 15:00:31 +0530

    v0.1.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [isDark, setIsDark] = useState(true);
+
+  const toggleTheme = () => {
+    setIsDark(!isDark);
+  };
+
   return (
-    <div className="App">
-      <h1>Initializing MemoFlux</h1>
+    <div className={`App ${isDark ? 'dark' : 'light'}`}>
+      <div className="bg_effect"></div>
+      <div className="wrapper">
+        <button
+          onClick={toggleTheme}
+          className={`toggle-button ${isDark ? 'dark' : 'light'}`}
+        >
+        </button>
+        <h1>Initializing MemoFlux</h1>
+      </div>
     </div>
   );
 }
